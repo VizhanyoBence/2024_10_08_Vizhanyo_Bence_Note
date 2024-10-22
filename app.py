cec687e4f41ca021e123f7072afd98b75b266ed9
VizhanyoBence <vizhanyo.bence@diak.szbi-pg.hu>
2024-10-22 09:43:59 +0200

    register, full name, email, password, phone number, gender, fav color, actual time, profile pic, born date, interest

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 products=[]
@@ -9,7 +9,10 @@ def index():
         name = request.form.get('product_name') 
         products.append(name)
     return render_template('index.html', products_=products)
-
-
+@app.route('/submit_products', methods=['POST'])
+def index():
+        name = request.form.get('product_name')
+        products.append(name)
+        return redirect(url_for('index'))
 if __name__== "__main__":
     app.run(debug=True)
